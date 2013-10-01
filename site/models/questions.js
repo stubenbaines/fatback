cbbb3296ccb0652f61f66d4a77a685b2b7b31ba1
Dennis Pierce <stubenbaines@gmail.com>
2013-10-01 00:01:41 -0400

    Fix to schema to remove id. Use mongodb's built in _id.

--- site/models/questions.js
+++ site/models/questions.js
@@ -4,7 +4,6 @@ var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
 var Question = new Schema({
-    id : {type : Number, require : true, trim : true, unique : true},
     title : {type : String, required : true, trim : true},
     body : String,
     tags : Array 
